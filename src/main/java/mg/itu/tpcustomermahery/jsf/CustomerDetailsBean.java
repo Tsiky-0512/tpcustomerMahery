6422ff8a5695312d2cac30c7de20c89bd4ade0e4
Mahery Rasolofomanana <tsiky.rasolofomanana@gmail.com>
2023-03-27 23:21:33 +0300

    Ajout de la classe Details pour la mise ajour , findByID d'un customer

---
+++ src/main/java/mg/itu/tpcustomermahery/jsf/CustomerDetailsBean.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpcustomermahery.jsf;
+
+import jakarta.ejb.EJB;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import mg.itu.tpcustomermahery.ejb.CustomerManager;
+import mg.itu.tpcustomermahery.entities.Customer;
+
+/**
+ *
+ * @author Peabody
+ */
+@Named
+@ViewScoped
+public class CustomerDetailsBean {
+    private int idCustomer;
+    private Customer customer;
+    
+    @EJB
+    private CustomerManager customerManager;
+    
+    public int getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(int idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+    
+    public Customer getCustomer() {
+      return customer;
+    }
+    
+    public String update() {
+        customer = customerManager.update(customer);
+        return "customerList";
+    }
+    
+    public void loadCustomer() {
+        this.customer = customerManager.findById(idCustomer);
+    }
+    
+}
